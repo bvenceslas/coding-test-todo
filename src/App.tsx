5c7dcc6e63cad60b863e8a7998497a54a433ea00
bvenceslas <bvenceslas@gmail.com>
2023-04-01 13:48:54 +0200

    Create new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,191 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
-type Task = {
+type Todo = {
   id: number;
   title: string;
   completed: boolean;
 };
 
-type SortOrder = "asc" | "desc";
-
 const App = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [sortField, setSortField] = useState<keyof Task>("id");
-  const [sortOrder, setSortOrder] = useState<SortOrder>("asc");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  const [sortField, setSortField] = useState<"id" | "title" | "completed">(
+    "id"
+  );
+  const [newTodoTitle, setNewTodoTitle] = useState("");
 
   useEffect(() => {
-    const fetchTasks = async () => {
+    const fetchTodos = async () => {
       try {
-        const response = await fetch(
+        const response = await axios.get<Todo[]>(
           "https://jsonplaceholder.typicode.com/todos"
         );
-        const data = await response.json();
-        const tasksData = data.map((task: Task) => ({
-          id: task.id,
-          title: task.title,
-          completed: task.completed,
-        }));
-        setTasks(tasksData);
+        setTodos(response.data);
         setIsLoading(false);
       } catch (error) {
         console.error(error);
       }
     };
-    fetchTasks();
+
+    fetchTodos();
   }, []);
 
-  const sortTasks = (field: keyof Task) => {
-    if (field === sortField) {
-      // toggle order if sorting same field
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  const handleNewTodoTitleChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewTodoTitle(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodoTitle === "") {
+      alert("A task cannot be empty");
+      return;
+    }
+    const newTodoId = Math.max(...todos.map((todo) => todo.id)) + 1;
+    const newTodo: Todo = {
+      id: newTodoId,
+      title: newTodoTitle,
+      completed: false,
+    };
+
+    const index = todos.findIndex((todo) => {
+      switch (sortField) {
+        case "id":
+          return sortDirection === "asc"
+            ? newTodoId < todo.id
+            : newTodoId > todo.id;
+        case "title":
+          return sortDirection === "asc"
+            ? newTodo.title.localeCompare(todo.title) === -1
+            : newTodo.title.localeCompare(todo.title) === 1;
+        case "completed":
+          return sortDirection === "asc"
+            ? newTodo.completed && !todo.completed
+            : todo.completed && !newTodo.completed;
+        default:
+          return false;
+      }
+    });
+
+    if (index === -1) {
+      setTodos([...todos, newTodo]);
     } else {
-      // set new field and ascending order
+      const updatedTodos = [...todos];
+      updatedTodos.splice(index, 0, newTodo);
+      setTodos(updatedTodos);
+    }
+    setNewTodoTitle("");
+  };
+
+  const toggleSortDirection = (field: "id" | "title" | "completed") => {
+    if (sortField === field) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
       setSortField(field);
-      setSortOrder("asc");
+      setSortDirection("asc");
     }
   };
 
-  const sortedTasks = tasks.sort((a, b) => {
-    const order = sortOrder === "asc" ? 1 : -1;
-    if (a[sortField] > b[sortField]) {
-      return order;
+  const handleCompleteTodo = (id: number) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const sortedTodos = todos.sort((a, b) => {
+    let aValue, bValue;
+
+    switch (sortField) {
+      case "id":
+        aValue = a.id;
+        bValue = b.id;
+        break;
+      case "title":
+        aValue = a.title;
+        bValue = b.title;
+        break;
+      case "completed":
+        aValue = a.completed;
+        bValue = b.completed;
+        break;
+      default:
+        aValue = a.id;
+        bValue = b.id;
     }
-    if (a[sortField] < b[sortField]) {
-      return -order;
+
+    if (aValue < bValue) {
+      return sortDirection === "asc" ? -1 : 1;
+    } else if (aValue > bValue) {
+      return sortDirection === "asc" ? 1 : -1;
+    } else {
+      return 0;
     }
-    return 0;
   });
 
   return (
-    <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          <li onClick={() => sortTasks("id")}>
-            <span>ID</span>
-            <span>{sortField === "id" && sortOrder === "asc" ? "a" : "c"}</span>
-          </li>
-          <li onClick={() => sortTasks("title")}>
-            <span>Name</span>
-            <span>
-              {sortField === "title" && sortOrder === "asc" ? "a" : "c"}
-            </span>
-          </li>
-          <li onClick={() => sortTasks("completed")}>
-            <span>Completed</span>
-            <span>
-              {sortField === "completed" && sortOrder === "asc" ? "a" : "c"}
-            </span>
-          </li>
-          {sortedTasks.map((task) => (
-            <li key={task.id}>
-              <span>{task.id}</span>
-              <span>{task.title}</span>
-              <span>{task.completed ? "completed" : "not completed"}</span>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="container">
+      <div className="App">
+        <h1>Todo App</h1>
+        <div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleAddTodo();
+            }}
+          >
+            <input
+              type="text"
+              value={newTodoTitle}
+              onChange={handleNewTodoTitleChange}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th onClick={() => toggleSortDirection("id")}>ID</th>
+                <th onClick={() => toggleSortDirection("title")}>Title</th>
+                <th onClick={() => toggleSortDirection("completed")}>
+                  Completed
+                </th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedTodos.map((todo) => (
+                <tr key={todo.id}>
+                  <td>{todo.id}</td>
+                  <td>{todo.title}</td>
+                  <td>{todo.completed ? "Yes" : "No"}</td>
+                  <td>
+                    {!todo.completed && (
+                      <button onClick={() => handleCompleteTodo(todo.id)}>
+                        Complete
+                      </button>
+                    )}
+                    <button onClick={() => handleDeleteTodo(todo.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 };
