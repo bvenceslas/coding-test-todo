b92bac7358cba45e66c5a1ba2809e256f2a7f3dd
bvenceslas <bvenceslas@gmail.com>
2023-04-01 11:06:26 +0200

    fetch data from json placeholder and list with id, title, and completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return <div className="App">To-Do App</div>;
-}
+type Task = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const data = await response.json();
+        const tasksData = data.map((task: Task) => ({
+          id: task.id,
+          title: task.title,
+          completed: task.completed,
+        }));
+        setTasks(tasksData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {tasks.map((task: Task) => (
+            <li key={task.id}>
+              <span>{task.id}</span>
+              <span>{task.title}</span>
+              <span>{task.completed ? "completed" : "not completed"}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default App;
