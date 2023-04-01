cc56ddf0796c10aef041713b8c9379607346ca3d
bvenceslas <bvenceslas@gmail.com>
2023-04-01 11:22:48 +0200

    Add sort algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 type Task = {
   id: number;
@@ -6,9 +7,13 @@ type Task = {
   completed: boolean;
 };
 
+type SortOrder = "asc" | "desc";
+
 const App = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortField, setSortField] = useState<keyof Task>("id");
+  const [sortOrder, setSortOrder] = useState<SortOrder>("asc");
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -31,13 +36,51 @@ const App = () => {
     fetchTasks();
   }, []);
 
+  const sortTasks = (field: keyof Task) => {
+    if (field === sortField) {
+      // toggle order if sorting same field
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      // set new field and ascending order
+      setSortField(field);
+      setSortOrder("asc");
+    }
+  };
+
+  const sortedTasks = tasks.sort((a, b) => {
+    const order = sortOrder === "asc" ? 1 : -1;
+    if (a[sortField] > b[sortField]) {
+      return order;
+    }
+    if (a[sortField] < b[sortField]) {
+      return -order;
+    }
+    return 0;
+  });
+
   return (
     <div>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         <ul>
-          {tasks.map((task: Task) => (
+          <li onClick={() => sortTasks("id")}>
+            <span>ID</span>
+            <span>{sortField === "id" && sortOrder === "asc" ? "a" : "c"}</span>
+          </li>
+          <li onClick={() => sortTasks("title")}>
+            <span>Name</span>
+            <span>
+              {sortField === "title" && sortOrder === "asc" ? "a" : "c"}
+            </span>
+          </li>
+          <li onClick={() => sortTasks("completed")}>
+            <span>Completed</span>
+            <span>
+              {sortField === "completed" && sortOrder === "asc" ? "a" : "c"}
+            </span>
+          </li>
+          {sortedTasks.map((task) => (
             <li key={task.id}>
               <span>{task.id}</span>
               <span>{task.title}</span>
